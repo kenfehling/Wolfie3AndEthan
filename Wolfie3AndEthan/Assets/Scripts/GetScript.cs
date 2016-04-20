4ed3a08ab48ebe36b58398b08f3ca75d5a7a50ef
Alec Day <shirojyuu@gmail.com>
2016-04-20 18:24:58 -0400

    Revert "Revert "Add everything.""
    This reverts commit 0bd99798c8f8067427ddccbfeeb5ee80528332ee.

---
+++ Wolfie3AndEthan/Assets/Scripts/GetScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GetScript : MonoBehaviour {
+
+	public Text victoryMessage;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		victoryMessage.text = "Win.";
+	}
+}
