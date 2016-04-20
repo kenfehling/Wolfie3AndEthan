1f2da5754c89ed1a1fa92e0a6c656baeee8afeeb
Alec Day <shirojyuu@gmail.com>
2016-04-20 18:17:05 -0400

    Add everything.

---
+++ Wolfie3AndEthan/Assets/Scripts/RotScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotScript : MonoBehaviour {
+
+	public float rotSpeed = 0.02f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotSpeed, 0.0f, 0.0f);
+	}
+}
